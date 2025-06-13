00c75064a8100ec5f4cf7ce6dafa6a2ee2ddfdec
James Pether Sörling <pethers@users.noreply.github.com>
2025-06-13 10:39:00 +0000

    add TrigramSystem tests for transition logic and stance effectiveness

--- src/systems/TrigramSystem.test.ts
+++ src/systems/TrigramSystem.test.ts
@@ -1,8 +1,8 @@
 import { describe, it, expect, beforeEach } from "vitest";
-import { TrigramSystem } from "../TrigramSystem";
-import { TrigramStance, PlayerArchetype } from "../../types/enums";
-import { createPlayerFromArchetype } from "../../utils/playerUtils";
-import type { PlayerState } from "../../types";
+import { TrigramSystem } from "./TrigramSystem";
+import { TrigramStance, PlayerArchetype } from "../types/enums";
+import { createPlayerFromArchetype } from "../utils/playerUtils";
+import type { PlayerState } from "../types";
 
 describe("TrigramSystem", () => {
   let system: TrigramSystem;
